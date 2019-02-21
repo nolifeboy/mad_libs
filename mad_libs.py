d324fac37d4fdbaec0764b2568c2fbd88172f16d
nolifeboy <47855818+nolifeboy@users.noreply.github.com>
2019-02-21 12:37:52 +0000

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,27 @@
+sin_noun = input("Enter a singular noun: ")
+sin_noun2 = input("Enter a second singular noun: ")
+sin_noun3 = input("Enter a third singular noun: ")
+name = input("Enter a name: ")
+relation = input("Enter a relation (parents, siblings, friends, etc): ")
+celeb_name = input("Enter a celebrity's name: ")
+place = input("Enter a place/location: ")
+verb = input("Enter a verb: ")
+gend = input("Enter a gender: ")
+
+print("\n")
+print("\n")
+
+print("There once was a " + sin_noun)
+print("It met a " + gend + " named " + name)
+print("They became best " + relation)
+print("But it wouldnt last long.....")
+print("Because " + celeb_name + " stole its " + sin_noun2)
+print("The " + sin_noun + " was devistated!!")
+print("So it went to (the) " + place + " to " + verb)
+print("But then it fell in love with a " + sin_noun3)
+
+print("\n")
+print("\n")
+
+#this is just so the program doesnt close as soon as the text appears
+input("Press ENTER to exit the program...")
